964a9b833e52d4365672c11e25c15986d9c6cecb
Benito Perdomo <benito.perdomo.v@gmail.com>
2023-09-22 10:54:15 -0600

    add cors permissions

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,25 @@ import routes from './routes/index';
 
 const app = express();
 app.use(json());
-app.use('/' + config.prefix, routes);
 
-// last middleware
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    // res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Methods', 'POST');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    } else {
+        return next();
+    }
+});
+
+
+app.use('/' + config.prefix, routes);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
-    console.log(`server is listening on port ${config.port}`);
+    console.log(`Servidor ejecutandose en el puerto: ${config.port}`);
 });
